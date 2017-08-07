ac5336275883b3c9a5c9f28036942cd014e66b2a
Manca Bizjak <manca.bizjak@xlab.si>
2017-08-07 07:32:10 +0200

    Enable log suppression
    This commit adds a TurnOff function to emmy's logging package. The function
    is useful for suppressing otherwise somewhat intentionally verbose logging,
    which is not desired when running tests or benchmarks.
    
    Signed-off-by: Manca Bizjak <manca.bizjak@xlab.si>

--- log/logging.go
+++ log/logging.go
@@ -2,6 +2,9 @@ package log
 
 import "github.com/op/go-logging"
 
+var clientModule = "client"
+var serverModule = "server"
+
 var longFormat = logging.MustStringFormatter(
 	`%{color}[%{module}] %{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
 )
@@ -16,4 +19,11 @@ func init() {
 	ClientLogger = logging.MustGetLogger("client")
 	ServerLogger = logging.MustGetLogger("server")
 	logging.SetFormatter(longFormat)
+}
+
+// TurnOffLogging sets the log level to -1, causing logs not to be printed to stdout.
+// This is useful to avoid otherwise verbose logging during benchmarks
+func TurnOff() {
+	logging.SetLevel(-1, clientModule)
+	logging.SetLevel(-1, serverModule)
 }
