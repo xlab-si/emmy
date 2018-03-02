5439ac4602fa2f6bdea0d6ccea600828fbca353d
Manca Bizjak <manca.bizjak@xlab.si>
2018-03-02 09:49:05 +0100

    Update CLI info
    This commit adds copyright and version information to the emmy CLI
    command. Version can be set with the new "release" target in the
    Makefile. In addition, the binary now holds information about its
    compilation date.
    
    Signed-off-by: Manca Bizjak <manca.bizjak@xlab.si>

--- emmy.go
+++ emmy.go
@@ -19,16 +19,26 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/urfave/cli"
 	emmy "github.com/xlab-si/emmy/cmd"
 )
 
+// version marks the version of emmy.
+// Its value should be set externally at compile time, by appending
+// -ldflags "-X main.version=x.y.z" option to go build/run/install commands at compile time.
+// In case its value remains empty, the CLI simply contains no version information.
+var version string
+
 // main runs the emmy CLI app.
 func main() {
 	app := cli.NewApp()
 	app.Name = "emmy"
-	app.Version = "0.1"
+	app.Copyright = `(c) 2017 XLAB d.o.o.
+		Licensed under Apache License, Version 2.0.`
+	app.Version = version
+	app.Compiled = time.Now()
 	app.Usage = `A CLI app for running emmy server, emmy clients 
 		and examples of proofs offered by the emmy library`
 	app.Commands = []cli.Command{emmy.ServerCmd, emmy.ClientCmd}
